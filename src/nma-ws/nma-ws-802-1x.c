GNOME/libnma

commit b61cc1b9fa040013a57430962d08a5316efff665

nma-ws: add
This is a library based on src/wireless-security that aims to replace
it, providing a public API in libnma.

The wireless-security code is used by the wifi-dialog provided by libnma,
nm-applet, nm-connection-editor and gnome-control-center, that should all be
ported to this common library.

src/nma-ws/nma-ws-802-1x.c
@@ -1,0 +1,528 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2007 - 2019 Red Hat, Inc.
+ */
+
+#include "nm-default.h"
+#include "nma-private.h"
+
+#include <ctype.h>
+#include <string.h>
+
+#include "nma-ws.h"
+#include "nma-ws-private.h"
+#include "nma-ws-802-1x.h"
+#include "nma-ws-802-1x-private.h"
+#include "nma-ws-helpers.h"
+#include "nma-ui-utils.h"
+
+#include "nma-eap-tls.h"
+#include "nma-eap-leap.h"
+#include "nma-eap-fast.h"
+#include "nma-eap-ttls.h"
+#include "nma-eap-peap.h"
+#include "nma-eap-simple.h"
+#include "nma-eap.h"
+
+#include "utils.h"
+
+#define AUTH_NAME_COLUMN   0
+#define AUTH_METHOD_COLUMN 1
+
+static void nma_ws_interface_init (NMAWsInterface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (NMAWs8021x, nma_ws_802_1x, GTK_TYPE_GRID,
+                         G_IMPLEMENT_INTERFACE (NMA_TYPE_WS, nma_ws_interface_init))
+
+enum {
+	PROP_0,
+	PROP_CONNECTION,
+	PROP_SECRETS_ONLY,
+	PROP_IS_EDITOR,
+	PROP_SECRETS_HINTS,
+	PROP_LAST
+};
+
+void
+nma_ws_802_1x_set_userpass (NMAWs8021x *self,
+                            const char *user,
+                            const char *password,
+                            gboolean always_ask,
+                            gboolean show_password)
+{
+	g_free (self->username);
+	self->username = g_strdup (user);
+
+	if (self->password) {
+		memset (self->password, 0, strlen (self->password));
+		g_free (self->password);
+	}
+	self->password = g_strdup (password);
+
+	if (always_ask != (gboolean) -1)
+		self->always_ask = always_ask;
+	self->show_password = show_password;
+}
+
+static void
+init_userpass (NMAWs8021x *self, NMConnection *connection)
+{
+	const char *user = NULL, *password = NULL;
+	gboolean always_ask = FALSE, show_password = FALSE;
+	NMSetting8021x  *setting;
+	NMSettingSecretFlags flags;
+
+	if (!connection)
+		goto set;
+
+	setting = nm_connection_get_setting_802_1x (connection);
+	if (!setting)
+		goto set;
+
+	user = nm_setting_802_1x_get_identity (setting);
+	password = nm_setting_802_1x_get_password (setting);
+
+	if (nm_setting_get_secret_flags (NM_SETTING (setting), NM_SETTING_802_1X_PASSWORD, &flags, NULL))
+		always_ask = !!(flags & NM_SETTING_SECRET_FLAG_NOT_SAVED);
+
+set:
+	nma_ws_802_1x_set_userpass (self, user, password, always_ask, show_password);
+}
+
+static gboolean
+validate (NMAWs *ws, GError **error)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (ws);
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	NMAEap *eap = NULL;
+	gboolean valid = FALSE;
+
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (self->eap_auth_combo));
+	gtk_combo_box_get_active_iter (GTK_COMBO_BOX (self->eap_auth_combo), &iter);
+	gtk_tree_model_get (model, &iter, AUTH_METHOD_COLUMN, &eap, -1);
+	g_return_val_if_fail (eap, FALSE);
+	valid = nma_eap_validate (eap, error);
+	nma_eap_unref (eap);
+	return valid;
+}
+
+static void
+auth_combo_changed_cb (GtkWidget *combo, gpointer user_data)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (user_data);
+
+	NMAEap *eap = NULL;
+	GList *elt, *children;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	GtkWidget *eap_widget;
+	GtkWidget *eap_default_widget = NULL;
+
+	/* Remove any previous wireless security widgets */
+	children = gtk_container_get_children (GTK_CONTAINER (self->eap_vbox));
+	for (elt = children; elt; elt = g_list_next (elt))
+		gtk_container_remove (GTK_CONTAINER (self->eap_vbox), GTK_WIDGET (elt->data));
+
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (combo));
+	gtk_combo_box_get_active_iter (GTK_COMBO_BOX (combo), &iter);
+	gtk_tree_model_get (model, &iter, AUTH_METHOD_COLUMN, &eap, -1);
+	g_return_if_fail (eap);
+
+	eap_widget = nma_eap_get_widget (eap);
+	g_return_if_fail (eap_widget);
+	gtk_widget_unparent (eap_widget);
+
+	gtk_container_add (GTK_CONTAINER (self->eap_vbox), eap_widget);
+
+	/* Refocus the EAP method's default widget */
+	if (eap->default_field) {
+		eap_default_widget = GTK_WIDGET (gtk_builder_get_object (eap->builder, eap->default_field));
+		if (eap_default_widget)
+			gtk_widget_grab_focus (eap_default_widget);
+	}
+
+	nma_eap_unref (eap);
+
+	nma_ws_changed_cb (combo, NMA_WS (self));
+}
+
+static void
+add_to_size_group (NMAWs *ws, GtkSizeGroup *group)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (ws);
+	NMAEap *eap = NULL;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (self->eap_auth_combo));
+
+	/* Let each EAP method try to update its secrets */
+	if (gtk_tree_model_get_iter_first (model, &iter)) {
+		do {
+			gtk_tree_model_get (model, &iter, AUTH_METHOD_COLUMN, &eap, -1);
+			if (eap) {
+				nma_eap_add_to_size_group (eap, group);
+				nma_eap_unref (eap);
+			}
+		} while (gtk_tree_model_iter_next (model, &iter));
+	}
+
+	gtk_size_group_add_widget (group, self->eap_auth_label);
+}
+
+void
+nma_ws_802_1x_fill_connection (NMAWs *ws, NMConnection *connection)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (ws);
+	NMSettingWirelessSecurity *s_wireless_sec;
+	NMSetting8021x *s_8021x;
+	NMAEap *eap = NULL;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+
+	/* Get the NMAEap object */
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (self->eap_auth_combo));
+	gtk_combo_box_get_active_iter (GTK_COMBO_BOX (self->eap_auth_combo), &iter);
+	gtk_tree_model_get (model, &iter, AUTH_METHOD_COLUMN, &eap, -1);
+	g_return_if_fail (eap);
+
+	/* Blow away the old wireless security setting by adding a clear one */
+	s_wireless_sec = (NMSettingWirelessSecurity *) nm_setting_wireless_security_new ();
+	nm_connection_add_setting (connection, (NMSetting *) s_wireless_sec);
+
+	/* Blow away the old 802.1x setting by adding a clear one */
+	s_8021x = (NMSetting8021x *) nm_setting_802_1x_new ();
+	nm_connection_add_setting (connection, (NMSetting *) s_8021x);
+
+	nma_eap_fill_connection (eap, connection);
+	nma_eap_unref (eap);
+}
+
+static void
+update_secrets (NMAWs *ws, NMConnection *connection)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (ws);
+	NMAEap *eap = NULL;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+
+	g_return_if_fail (connection != NULL);
+
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (self->eap_auth_combo));
+
+	/* Let each EAP method try to update its secrets */
+	if (gtk_tree_model_get_iter_first (model, &iter)) {
+		do {
+			gtk_tree_model_get (model, &iter, AUTH_METHOD_COLUMN, &eap, -1);
+			if (eap) {
+				nma_eap_update_secrets (eap, connection);
+				nma_eap_unref (eap);
+			}
+		} while (gtk_tree_model_iter_next (model, &iter));
+	}
+}
+
+static void
+get_property (GObject *object,
+              guint prop_id,
+              GValue *value,
+              GParamSpec *pspec)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (object);
+
+	switch (prop_id) {
+	case PROP_CONNECTION:
+		g_value_set_object (value, self->connection);
+		break;
+	case PROP_SECRETS_ONLY:
+		g_value_set_boolean (value, self->secrets_only);
+		break;
+	case PROP_IS_EDITOR:
+		g_value_set_boolean (value, self->is_editor);
+		break;
+	case PROP_SECRETS_HINTS:
+		g_value_set_boxed (value, self->secrets_hints);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+set_property (GObject *object,
+              guint prop_id,
+              const GValue *value,
+              GParamSpec *pspec)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (object);
+
+	switch (prop_id) {
+	case PROP_CONNECTION:
+		self->connection = g_value_dup_object (value);
+		break;
+	case PROP_SECRETS_ONLY:
+		self->secrets_only = g_value_get_boolean (value);
+		break;
+	case PROP_IS_EDITOR:
+		self->is_editor = g_value_get_boolean (value);
+		break;
+	case PROP_SECRETS_HINTS:
+		self->secrets_hints = g_value_dup_boxed (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+nma_ws_802_1x_init (NMAWs8021x *self)
+{
+	gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+static void
+nma_ws_interface_init (NMAWsInterface *iface)
+{
+	iface->validate = validate;
+	iface->add_to_size_group = add_to_size_group;
+	iface->fill_connection = nma_ws_802_1x_fill_connection;
+	iface->update_secrets = update_secrets;
+	iface->adhoc_compatible = FALSE;
+	iface->hotspot_compatible = FALSE;
+}
+
+static void
+constructed (GObject *object)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (object);
+	GtkListStore *auth_model;
+	GtkTreeIter iter;
+	NMAEapSimple *em_md5;
+	NMAEapTls *em_tls;
+	NMAEapLeap *em_leap;
+	NMAEapSimple *em_pwd;
+	NMAEapFast *em_fast;
+	NMAEapTtls *em_ttls;
+	NMAEapPeap *em_peap;
+	const char *default_method = NULL, *ctype = NULL;
+	int active = -1, item = 0;
+	gboolean wired = FALSE;
+	NMAEapSimpleFlags simple_flags = NMA_EAP_SIMPLE_FLAG_NONE;
+
+	/* Grab the default EAP method out of the security object */
+	if (self->connection) {
+		NMSettingConnection *s_con;
+		NMSetting8021x *s_8021x;
+
+		s_con = nm_connection_get_setting_connection (self->connection);
+		if (s_con)
+			ctype = nm_setting_connection_get_connection_type (s_con);
+		if (   (g_strcmp0 (ctype, NM_SETTING_WIRED_SETTING_NAME) == 0)
+		    || nm_connection_get_setting_wired (self->connection))
+			wired = TRUE;
+
+		s_8021x = nm_connection_get_setting_802_1x (self->connection);
+		if (s_8021x && nm_setting_802_1x_get_num_eap_methods (s_8021x))
+			default_method = nm_setting_802_1x_get_eap_method (s_8021x, 0);
+	}
+
+	/* initialize NMAWs userpass from connection (clear if no connection) */
+	init_userpass (self, self->connection);
+
+	auth_model = gtk_list_store_new (2, G_TYPE_STRING, nma_eap_get_type ());
+
+	if (self->is_editor)
+		simple_flags |= NMA_EAP_SIMPLE_FLAG_IS_EDITOR;
+	if (self->secrets_only)
+		simple_flags |= NMA_EAP_SIMPLE_FLAG_SECRETS_ONLY;
+
+	if (wired) {
+		em_md5 = nma_eap_simple_new (self, self->connection, NMA_EAP_SIMPLE_TYPE_MD5, simple_flags, NULL);
+		gtk_list_store_append (auth_model, &iter);
+		gtk_list_store_set (auth_model, &iter,
+		                    AUTH_NAME_COLUMN, _("MD5"),
+		                    AUTH_METHOD_COLUMN, em_md5,
+		                    -1);
+		nma_eap_unref (NMA_EAP (em_md5));
+		if (default_method && (active < 0) && !strcmp (default_method, "md5"))
+			active = item;
+		item++;
+	}
+
+	em_tls = nma_eap_tls_new (self, self->connection, FALSE, self->secrets_only);
+	gtk_list_store_append (auth_model, &iter);
+	gtk_list_store_set (auth_model, &iter,
+	                    AUTH_NAME_COLUMN, _("TLS"),
+	                    AUTH_METHOD_COLUMN, em_tls,
+	                    -1);
+	nma_eap_unref (NMA_EAP (em_tls));
+	if (default_method && (active < 0) && !strcmp (default_method, "tls"))
+		active = item;
+	item++;
+
+	if (!wired) {
+		em_leap = nma_eap_leap_new (self, self->connection, self->secrets_only);
+		gtk_list_store_append (auth_model, &iter);
+		gtk_list_store_set (auth_model, &iter,
+		                    AUTH_NAME_COLUMN, _("LEAP"),
+		                    AUTH_METHOD_COLUMN, em_leap,
+		                    -1);
+		nma_eap_unref (NMA_EAP (em_leap));
+		if (default_method && (active < 0) && !strcmp (default_method, "leap"))
+			active = item;
+		item++;
+	}
+
+	em_pwd = nma_eap_simple_new (self, self->connection, NMA_EAP_SIMPLE_TYPE_PWD, simple_flags, NULL);
+	gtk_list_store_append (auth_model, &iter);
+	gtk_list_store_set (auth_model, &iter,
+	                    AUTH_NAME_COLUMN, _("PWD"),
+	                    AUTH_METHOD_COLUMN, em_pwd,
+	                    -1);
+	nma_eap_unref (NMA_EAP (em_pwd));
+	if (default_method && (active < 0) && !strcmp (default_method, "pwd"))
+		active = item;
+	item++;
+
+	em_fast = nma_eap_fast_new (self, self->connection, self->is_editor, self->secrets_only);
+	gtk_list_store_append (auth_model, &iter);
+	gtk_list_store_set (auth_model, &iter,
+	                    AUTH_NAME_COLUMN, _("FAST"),
+	                    AUTH_METHOD_COLUMN, em_fast,
+	                    -1);
+	nma_eap_unref (NMA_EAP (em_fast));
+	if (default_method && (active < 0) && !strcmp (default_method, "fast"))
+		active = item;
+	item++;
+
+	em_ttls = nma_eap_ttls_new (self, self->connection, self->is_editor, self->secrets_only);
+	gtk_list_store_append (auth_model, &iter);
+	gtk_list_store_set (auth_model, &iter,
+	                    AUTH_NAME_COLUMN, _("Tunneled TLS"),
+	                    AUTH_METHOD_COLUMN, em_ttls,
+	                    -1);
+	nma_eap_unref (NMA_EAP (em_ttls));
+	if (default_method && (active < 0) && !strcmp (default_method, "ttls"))
+		active = item;
+	item++;
+
+	em_peap = nma_eap_peap_new (self, self->connection, self->is_editor, self->secrets_only);
+	gtk_list_store_append (auth_model, &iter);
+	gtk_list_store_set (auth_model, &iter,
+	                    AUTH_NAME_COLUMN, _("Protected EAP (PEAP)"),
+	                    AUTH_METHOD_COLUMN, em_peap,
+	                    -1);
+	nma_eap_unref (NMA_EAP (em_peap));
+	if (default_method && (active < 0) && !strcmp (default_method, "peap"))
+		active = item;
+	item++;
+
+	if (self->secrets_hints && self->secrets_hints[0]) {
+		NMAEapSimple *em_hints;
+
+		em_hints = nma_eap_simple_new (self, self->connection, NMA_EAP_SIMPLE_TYPE_UNKNOWN,
+		                               simple_flags, (const char **)self->secrets_hints);
+		gtk_list_store_append (auth_model, &iter);
+		gtk_list_store_set (auth_model, &iter,
+		                    AUTH_NAME_COLUMN, _("Unknown"),
+		                    AUTH_METHOD_COLUMN, em_hints,
+		                    -1);
+		nma_eap_unref (NMA_EAP (em_hints));
+		active = item;
+		item++;
+	} else if (default_method && !strcmp (default_method, "external")) {
+		NMAEapSimple *em_extern;
+		const char *empty_hints[] = { NULL };
+
+		em_extern = nma_eap_simple_new (self, self->connection, NMA_EAP_SIMPLE_TYPE_UNKNOWN,
+		                                simple_flags, empty_hints);
+		gtk_list_store_append (auth_model, &iter);
+		gtk_list_store_set (auth_model, &iter,
+		                    AUTH_NAME_COLUMN, _("Externally configured"),
+		                    AUTH_METHOD_COLUMN, em_extern,
+		                    -1);
+		nma_eap_unref (NMA_EAP (em_extern));
+			active = item;
+		item++;
+	}
+
+	gtk_combo_box_set_model (GTK_COMBO_BOX (self->eap_auth_combo), GTK_TREE_MODEL (auth_model));
+	g_object_unref (G_OBJECT (auth_model));
+	gtk_combo_box_set_active (GTK_COMBO_BOX (self->eap_auth_combo), active < 0 ? 0 : (guint32) active);
+
+	if (self->secrets_only) {
+		gtk_widget_hide (self->eap_auth_combo);
+		gtk_widget_hide (self->eap_auth_label);
+	}
+
+	G_OBJECT_CLASS (nma_ws_802_1x_parent_class)->constructed (object);
+}
+
+NMAWs8021x *
+nma_ws_802_1x_new (NMConnection *connection,
+                   gboolean is_editor,
+                   gboolean secrets_only)
+{
+	return g_object_new (NMA_TYPE_WS_802_1X,
+	                     "connection", connection,
+	                     "is-editor", is_editor,
+	                     "secrets-only", secrets_only,
+	                     NULL);
+}
+
+static void
+dispose (GObject *object)
+{
+	NMAWs8021x *self = NMA_WS_802_1X (object);
+
+	g_clear_object (&self->connection);
+	g_clear_pointer (&self->secrets_hints, g_strfreev);
+	g_clear_pointer (&self->username, g_free);
+	g_clear_pointer (&self->password, g_free);
+}
+
+static void
+nma_ws_802_1x_class_init (NMAWs8021xClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+	object_class->get_property = get_property;
+	object_class->set_property = set_property;
+	object_class->constructed = constructed;
+	object_class->dispose = dispose;
+
+	g_object_class_override_property (object_class,
+	                                  PROP_CONNECTION, "connection");
+
+	g_object_class_override_property (object_class,
+	                                  PROP_SECRETS_ONLY, "secrets-only");
+
+	g_object_class_install_property
+		(object_class, PROP_IS_EDITOR,
+		 g_param_spec_boolean ("is-editor", "", "",
+		                       FALSE,
+		                         G_PARAM_READWRITE
+		                       | G_PARAM_CONSTRUCT
+		                       | G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property
+		(object_class, PROP_SECRETS_HINTS,
+		 g_param_spec_boxed ("secrets-hints", "", "",
+		                     G_TYPE_STRV,
+		                       G_PARAM_READWRITE
+		                     | G_PARAM_CONSTRUCT
+		                     | G_PARAM_STATIC_STRINGS));
+
+	gtk_widget_class_set_template_from_resource (widget_class,
+	                                             "/org/gnome/libnma/nma-ws-802-1x.ui");
+
+	gtk_widget_class_bind_template_child (widget_class, NMAWs8021x, eap_auth_combo);
+	gtk_widget_class_bind_template_child (widget_class, NMAWs8021x, eap_auth_label);
+	gtk_widget_class_bind_template_child (widget_class, NMAWs8021x, eap_vbox);
+
+	gtk_widget_class_bind_template_callback (widget_class, auth_combo_changed_cb);
+}

src/nma-ws/nma-ws-leap.c
@@ -1,0 +1,293 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Dan Williams <dcbw@redhat.com>
+ *
+ * Copyright 2007 - 2019 Red Hat, Inc.
+ */
+
+#include "nm-default.h"
+#include "nma-private.h"
+
+#include <ctype.h>
+#include <string.h>
+
+#include "nma-ws.h"
+#include "nma-ws-private.h"
+#include "nma-ws-leap.h"
+#include "nma-ws-helpers.h"
+#include "nma-ui-utils.h"
+#include "utils.h"
+
+typedef struct {
+	GtkGridClass parent;
+} NMAWsLeapClass;
+
+struct _NMAWsLeap {
+	GtkGrid parent;
+
+	GtkWidget *leap_username_entry;
+	GtkWidget *leap_password_entry;
+	GtkWidget *leap_username_label;
+	GtkWidget *leap_password_label;
+	GtkWidget *show_checkbutton_leap;
+
+	NMConnection *connection;
+	gboolean secrets_only;
+};
+
+static void nma_ws_interface_init (NMAWsInterface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (NMAWsLeap, nma_ws_leap, GTK_TYPE_GRID,
+                         G_IMPLEMENT_INTERFACE (NMA_TYPE_WS, nma_ws_interface_init))
+
+enum {
+	PROP_0,
+	PROP_CONNECTION,
+	PROP_SECRETS_ONLY,
+	PROP_LAST
+};
+
+static void
+show_toggled_cb (GtkCheckButton *button, gpointer user_data)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (user_data);
+	gboolean visible;
+
+	visible = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (button));
+	gtk_entry_set_visibility (GTK_ENTRY (self->leap_password_entry), visible);
+}
+
+static gboolean
+validate (NMAWs *ws, GError **error)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (ws);
+	NMSettingSecretFlags secret_flags;
+	const char *text;
+	gboolean ret = TRUE;
+
+	text = gtk_editable_get_text (GTK_EDITABLE (self->leap_username_entry));
+	if (!text || !strlen (text)) {
+		widget_set_error (self->leap_username_entry);
+		g_set_error_literal (error, NMA_ERROR, NMA_ERROR_GENERIC, _("missing leap-username"));
+		ret = FALSE;
+	} else {
+		widget_unset_error (self->leap_username_entry);
+	}
+
+	secret_flags = nma_utils_menu_to_secret_flags (self->leap_password_entry);
+	text = gtk_editable_get_text (GTK_EDITABLE (self->leap_password_entry));
+
+	if (   secret_flags & NM_SETTING_SECRET_FLAG_NOT_SAVED
+	    || secret_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED
+	    || (text && strlen (text))) {
+		widget_unset_error (self->leap_password_entry);
+	} else {
+		widget_set_error (self->leap_password_entry);
+		if (ret) {
+			g_set_error_literal (error, NMA_ERROR, NMA_ERROR_GENERIC, _("missing leap-password"));
+			ret = FALSE;
+		}
+	}
+
+	return ret;
+}
+
+static void
+add_to_size_group (NMAWs *ws, GtkSizeGroup *group)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (ws);
+
+	gtk_size_group_add_widget (group, self->leap_username_label);
+	gtk_size_group_add_widget (group, self->leap_password_label);
+}
+
+static void
+fill_connection (NMAWs *ws, NMConnection *connection)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (ws);
+	NMSettingWirelessSecurity *s_wireless_sec;
+	NMSettingSecretFlags secret_flags;
+	const char *leap_password = NULL, *leap_username = NULL;
+
+	/* Blow away the old security setting by adding a clear one */
+	s_wireless_sec = (NMSettingWirelessSecurity *) nm_setting_wireless_security_new ();
+	nm_connection_add_setting (connection, (NMSetting *) s_wireless_sec);
+
+	leap_username = gtk_editable_get_text (GTK_EDITABLE (self->leap_username_entry));
+	leap_password = gtk_editable_get_text (GTK_EDITABLE (self->leap_password_entry));
+
+	g_object_set (s_wireless_sec,
+	              NM_SETTING_WIRELESS_SECURITY_KEY_MGMT, "ieee8021x",
+	              NM_SETTING_WIRELESS_SECURITY_AUTH_ALG, "leap",
+	              NM_SETTING_WIRELESS_SECURITY_LEAP_USERNAME, leap_username,
+	              NM_SETTING_WIRELESS_SECURITY_LEAP_PASSWORD, leap_password,
+	              NULL);
+
+	/* Save LEAP_PASSWORD_FLAGS to the connection */
+	secret_flags = nma_utils_menu_to_secret_flags (self->leap_password_entry);
+	nm_setting_set_secret_flags (NM_SETTING (s_wireless_sec), NM_SETTING_WIRELESS_SECURITY_LEAP_PASSWORD,
+	                             secret_flags, NULL);
+
+	/* Update secret flags and popup when editing the connection */
+	if (!self->secrets_only) {
+		nma_utils_update_password_storage (self->leap_password_entry, secret_flags,
+		                                   NM_SETTING (s_wireless_sec),
+		                                   NM_SETTING_WIRELESS_SECURITY_LEAP_PASSWORD);
+	}
+}
+
+static void
+update_secrets (NMAWs *ws, NMConnection *connection)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (ws);
+
+	nma_ws_helper_fill_secret_entry (connection,
+	                                 GTK_EDITABLE (self->leap_password_entry),
+	                                 NM_TYPE_SETTING_WIRELESS_SECURITY,
+	                                 (HelperSecretFunc) nm_setting_wireless_security_get_leap_password);
+}
+
+static void
+get_property (GObject *object,
+              guint prop_id,
+              GValue *value,
+              GParamSpec *pspec)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (object);
+
+	switch (prop_id) {
+	case PROP_CONNECTION:
+		g_value_set_object (value, self->connection);
+		break;
+	case PROP_SECRETS_ONLY:
+		g_value_set_boolean (value, self->secrets_only);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+set_property (GObject *object,
+              guint prop_id,
+              const GValue *value,
+              GParamSpec *pspec)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (object);
+
+	switch (prop_id) {
+	case PROP_CONNECTION:
+		self->connection = g_value_dup_object (value);
+		break;
+	case PROP_SECRETS_ONLY:
+		self->secrets_only = g_value_get_boolean (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+nma_ws_leap_init (NMAWsLeap *self)
+{
+	gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+static void
+nma_ws_interface_init (NMAWsInterface *iface)
+{
+	iface->validate = validate;
+	iface->add_to_size_group = add_to_size_group;
+	iface->fill_connection = fill_connection;
+	iface->update_secrets = update_secrets;
+	iface->adhoc_compatible = FALSE;
+	iface->hotspot_compatible = FALSE;
+}
+
+static void
+constructed (GObject *object)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (object);
+	NMSettingWirelessSecurity *wsec = NULL;
+
+	if (self->connection) {
+		wsec = nm_connection_get_setting_wireless_security (self->connection);
+		if (wsec) {
+			const char *auth_alg;
+
+			/* Ignore if wireless security doesn't specify LEAP */
+			auth_alg = nm_setting_wireless_security_get_auth_alg (wsec);
+			if (!auth_alg || strcmp (auth_alg, "leap"))
+				wsec = NULL;
+		}
+	}
+
+	/* Create password-storage popup menu for password entry under entry's secondary icon */
+	nma_utils_setup_password_storage (self->leap_password_entry, 0, (NMSetting *) wsec,
+	                                  NM_SETTING_WIRELESS_SECURITY_LEAP_PASSWORD,
+	                                  FALSE, self->secrets_only);
+
+	if (wsec) {
+		update_secrets (NMA_WS (self), self->connection);
+		gtk_editable_set_text (GTK_EDITABLE (self->leap_username_entry),
+		                       nm_setting_wireless_security_get_leap_username (wsec));
+	}
+
+	if (self->secrets_only) {
+		gtk_widget_hide (self->leap_username_label);
+		gtk_widget_hide (self->leap_username_entry);
+	}
+
+	gtk_widget_grab_focus (self->leap_password_entry);
+
+	G_OBJECT_CLASS (nma_ws_leap_parent_class)->constructed (object);
+}
+
+NMAWsLeap *
+nma_ws_leap_new (NMConnection *connection, gboolean secrets_only)
+{
+	return g_object_new (NMA_TYPE_WS_LEAP,
+	                     "connection", connection,
+	                     "secrets-only", secrets_only,
+	                     NULL);
+}
+
+static void
+dispose (GObject *object)
+{
+	NMAWsLeap *self = NMA_WS_LEAP (object);
+
+	g_clear_object (&self->connection);
+}
+
+static void
+nma_ws_leap_class_init (NMAWsLeapClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+	object_class->get_property = get_property;
+	object_class->set_property = set_property;
+	object_class->constructed = constructed;
+	object_class->dispose = dispose;
+
+	g_object_class_override_property (object_class,
+	                                  PROP_CONNECTION, "connection");
+
+	g_object_class_override_property (object_class,
+	                                  PROP_SECRETS_ONLY, "secrets-only");
+
+        gtk_widget_class_set_template_from_resource (widget_class,
+                                                     "/org/gnome/libnma/nma-ws-leap.ui");
+
+	gtk_widget_class_bind_template_child (widget_class, NMAWsLeap, leap_username_entry);
+	gtk_widget_class_bind_template_child (widget_class, NMAWsLeap, leap_password_entry);
+	gtk_widget_class_bind_template_child (widget_class, NMAWsLeap, leap_username_label);
+	gtk_widget_class_bind_template_child (widget_class, NMAWsLeap, leap_password_label);
+	gtk_widget_class_bind_template_child (widget_class, NMAWsLeap, show_checkbutton_leap);
+
+	gtk_widget_class_bind_template_callback (widget_class, nma_ws_changed_cb);
+	gtk_widget_class_bind_template_callback (widget_class, show_toggled_cb);
+}

commit c901762a4c310bc651590df11152d2fbbf5aa9c1

nma-ws: properly dispose wireless security objects
Chain up to parent to properly free resources on dispose().

Fixes: b61cc1b9fa040 ("nma-ws: add")

https://gitlab.gnome.org/GNOME/libnma/-/issues/7

src/nma-ws/nma-ws-802-1x.c
@@ -482,6 +482,8 @@ dispose (GObject *object)
 	g_clear_pointer (&self->secrets_hints, g_strfreev);
 	g_clear_pointer (&self->username, g_free);
 	g_clear_pointer (&self->password, g_free);
+
+	G_OBJECT_CLASS (nma_ws_802_1x_parent_class)->dispose (object);
 }
 
 static void

src/nma-ws/nma-ws-leap.c
@@ -260,6 +260,8 @@ dispose (GObject *object)
 	NMAWsLeap *self = NMA_WS_LEAP (object);
 
 	g_clear_object (&self->connection);
+
+	G_OBJECT_CLASS (nma_ws_leap_parent_class)->dispose (object);
 }
 
 static void

src/nma-ws/nma-ws-sae.c
@@ -252,6 +252,8 @@ dispose (GObject *object)
 	NMAWsSae *self = NMA_WS_SAE (object);
 
 	g_clear_object (&self->connection);
+
+	G_OBJECT_CLASS (nma_ws_sae_parent_class)->dispose (object);
 }
 
 static void

src/nma-ws/nma-ws-wep-key.c
@@ -404,6 +404,8 @@ dispose (GObject *object)
 	NMAWsWepKey *self = NMA_WS_WEP_KEY (object);
 
 	g_clear_object (&self->connection);
+
+	G_OBJECT_CLASS (nma_ws_wep_key_parent_class)->dispose (object);
 }
 
 static void

src/nma-ws/nma-ws-wpa-psk.c
@@ -270,6 +270,8 @@ dispose (GObject *object)
 	NMAWsWpaPsk *self = NMA_WS_WPA_PSK (object);
 
 	g_clear_object (&self->connection);
+
+	G_OBJECT_CLASS (nma_ws_wpa_psk_parent_class)->dispose (object);
 }
 
 static void