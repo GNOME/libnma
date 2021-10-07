25821fee93fce6865b86b60bd65f253f6b16de84
Lubomir Rintel <lkundrak@v3.sk>
2021-10-07 12:11:21 +0200

    tests/mobile-wizard: make it work with Gtk4
    gtk_main() and gtk_main_quit() are gone. Replace them with GMainLoop
    instead.

--- src/tests/mobile-wizard.c
+++ src/tests/mobile-wizard.c
@@ -12,19 +12,24 @@
 static void
 wizard_cb (NMAMobileWizard *self, gboolean canceled, NMAMobileWizardAccessMethod *method, gpointer user_data)
 {
-	gtk_main_quit ();
+	GMainLoop *loop = user_data;
+
+	g_main_loop_quit (loop);
 }
 
 int
 main (int argc, char *argv[])
 {
+	GMainLoop *loop;
 	NMAMobileWizard *wizard;
 
 	gtk_init ();
+	loop = g_main_loop_new (NULL, FALSE);
 
-	wizard = nma_mobile_wizard_new (NULL, NULL, NM_DEVICE_MODEM_CAPABILITY_NONE, TRUE, wizard_cb, NULL);
+	wizard = nma_mobile_wizard_new (NULL, NULL, NM_DEVICE_MODEM_CAPABILITY_NONE, TRUE, wizard_cb, loop);
 
 	nma_mobile_wizard_present (wizard);
-	gtk_main ();
+	g_main_loop_run (loop);
 	nma_mobile_wizard_destroy (wizard);
+	g_main_loop_unref (loop);
 }
