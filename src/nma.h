bbd7b5807a051b08c1f59cda4ff9e011d3eaacfc
Corentin Noël <corentin.noel@collabora.com>
2024-02-07 10:23:24 +0100

    Add single header to consume the library
    Allows to easily use libnma(-gtk4) by only using one header.
    Also makes the Vala bindings usable.

---
+++ src/nma.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: LGPL-2.1+
+/* NetworkManager Applet -- allow user control over networking
+ *
+ * Corentin Noël <corentin.noel@collabora.com>
+ *
+ * Copyright 2023 Collabora, Ltd.
+ */
+
+#ifndef __NMA_H__
+#define __NMA_H__
+
+#include <nma-bar-code.h>
+#include <nma-bar-code-widget.h>
+#include <nma-cert-chooser.h>
+#include <nma-mobile-providers.h>
+#include <nma-mobile-wizard.h>
+#include <nma-ui-utils.h>
+#include <nma-vpn-password-dialog.h>
+#include <nma-wifi-dialog.h>
+#include <nma-ws.h>
+#include <nma-ws-802-1x.h>
+#include <nma-ws-dynamic-wep.h>
+#include <nma-ws-leap.h>
+#include <nma-ws-owe.h>
+#include <nma-ws-sae.h>
+#include <nma-ws-wep-key.h>
+#include <nma-ws-wpa-eap.h>
+#include <nma-ws-wpa-psk.h>
+
+#endif /* __NMA_H__ */
